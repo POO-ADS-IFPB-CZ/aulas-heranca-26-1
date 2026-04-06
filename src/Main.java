ff2fe4a263a4317c451543a9f3a69e823b378f1c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-04-06 09:01:38 -0300

    instanciando as classes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
+import modelo.Funcionario;
+import modelo.Professor;
+import modelo.Tecnico;
 
 void main() {
+
+    Funcionario funcionario = new Professor("João",
+            "111.111.111-01",
+            LocalDate.of(2000,2,3),
+            "Rua Z, 300, Cajazeiras",
+            123, 2000, "Mestrado");
+    Tecnico funcionario1 = new Tecnico("Maria",
+            "222.222.222-02",
+            LocalDate.of(2000,3,3),
+            "Rua Y, 100, Cajazeiras",
+            312, 2000, "Laboratório de química");
 
 }
