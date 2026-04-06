41c6eb4a096efb3711ea446c934eccf74f9a3d0f
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-04-06 09:27:07 -0300

    Exemplo com polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,13 @@ void main() {
             "111.111.111-01",
             LocalDate.of(2000,2,3),
             "Rua Z, 300, Cajazeiras",
-            123, 2000, "Mestrado");
+            123, 2000, "Doutorado");
     Tecnico funcionario1 = new Tecnico("Maria",
             "222.222.222-02",
             LocalDate.of(2000,3,3),
             "Rua Y, 100, Cajazeiras",
             312, 2000, "Laboratório de química");
+    System.out.println(funcionario.calcularPagamento());
+    System.out.println(funcionario1.calcularPagamento());
 
 }
