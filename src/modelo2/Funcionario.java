fbfa08f9243173c2cfec06c1466bb87e12902b2c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-04-13 07:53:23 -0300

    Adicionando exemplo de funcionarios

---
+++ src/modelo2/Funcionario.java
@@ -0,0 +1,13 @@
+package modelo2;
+
+import java.time.LocalDate;
+
+public abstract class Funcionario {
+    private String cpf;
+    private String nome;
+    private LocalDate nascimento;
+    private int matricula;
+
+
+
+}
