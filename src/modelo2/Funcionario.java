4b1091de38983d8ce21254b6aac244fbf16961fe
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-04-13 07:56:01 -0300

    Criando métodos de funcionário

--- src/modelo2/Funcionario.java
+++ src/modelo2/Funcionario.java
@@ -8,6 +8,45 @@ public abstract class Funcionario {
     private LocalDate nascimento;
     private int matricula;
 
+    public Funcionario(String cpf, String nome, LocalDate nascimento, int matricula) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.matricula = matricula;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public abstract float calcularPagamento();
 
 }
