713b414b60c6208b7fe4c37fb61e1c369f8bd6e9
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-04-13 08:05:23 -0300

    adicionando classe horista

--- src/modelo2/Horista.java
+++ src/modelo2/Horista.java
@@ -1,4 +1,37 @@
 package modelo2;
 
-public class Horista {
+import java.time.LocalDate;
+
+public class Horista extends Funcionario{
+
+    private float valorHora;
+    private float totalHoras;
+
+    public Horista(String cpf, String nome, LocalDate nascimento, int matricula, float valorHora, float totalHoras) {
+        super(cpf, nome, nascimento, matricula);
+        this.valorHora = valorHora;
+        this.totalHoras = totalHoras;
+    }
+
+    public float getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(float valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public float getTotalHoras() {
+        return totalHoras;
+    }
+
+    public void setTotalHoras(float totalHoras) {
+        this.totalHoras = totalHoras;
+    }
+
+    @Override
+    public float calcularPagamento(){
+        return totalHoras*valorHora;
+    }
+
 }
